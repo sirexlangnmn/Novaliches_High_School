9da4833e015fa58b7480c0706f4884c1faa71e79
Federex  A  Potolin <potolin.federex@gmail.com>
2026-05-15 23:59:42 +0800

    print form 137

--- app/src/server.js
+++ app/src/server.js
@@ -426,6 +426,42 @@ app.get(['/form-137-ui'], (req, res) => {
 });
 
 
+
+app.get(['/students'], (req, res) => {
+    if (req.session.user === undefined) {
+        const sessionData = {
+            uuid: '',
+            type: '',
+            first_name: '',
+            last_name: '',
+            email: '',
+            country: '',
+            state_or_province: '',
+            ourGenerateNonce: lodashNonce,
+        };
+
+        res.render(path.join(__dirname, '../../', 'public/view/students/students'), {
+            data: sessionData,
+        });
+    } else {
+        const sessionData = {
+            uuid: req.session.user.uuid,
+            type: req.session.user.type,
+            first_name: req.session.user.first_name,
+            last_name: req.session.user.last_name,
+            email: req.session.user.email_or_social_media,
+            country: req.session.user.country,
+            state_or_province: req.session.user.state_or_province,
+            ourGenerateNonce: lodashNonce,
+        };
+
+        res.render(path.join(__dirname, '../../', 'public/view/students/students'), {
+            data: sessionData,
+        });
+    }
+});
+
+
 const { readFileSync, writeFile } = require('fs');
 const { check } = require('express-validator');
 
